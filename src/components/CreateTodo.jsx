fbb4eca87f5f98a723d28bf6b2cbe1c51b090d48
Dam <shit2wice@gmail.com>
2022-12-07 17:46:33 +0100

    style: Add new todo

--- src/components/CreateTodo.jsx
+++ src/components/CreateTodo.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const CreateTodo = () => {
-  return <div>CreateTodo</div>;
+  const date = new Date();
+  console.log(date);
+  return (
+    <form className="flex flex-col gap-4">
+      <label htmlFor="title">Title</label>
+      <input
+        type="text"
+        placeholder="Enter new task"
+        className="rounded-md outline-none p-1"
+        id="title"
+      />
+      <label htmlFor="description">Description</label>
+      <textarea
+        placeholder="description(optional)"
+        className="rounded-md outline-none p-1"
+        id="description"
+      />
+      <label htmlFor="date">Date</label>
+      <div id="date">07/12/2022</div>
+      <button type="submit" className="bg-blue-500 p-2 rounded-md">
+        Submit
+      </button>
+    </form>
+  );
 };
 
 export default CreateTodo;
