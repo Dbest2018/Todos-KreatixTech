152c5b92f67cdcc4f787f6b8339cfccf25cf5bbc
Dam <shit2wice@gmail.com>
2022-12-07 17:45:11 +0100

    style: Add main layout styles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import CreateTodo from "./components/CreateTodo";
 
 function App() {
   return (
-    <div>
-      <TodoList />
-      <CreateTodo />
+    <div className="flex flex-col items-center bg-slate-400 h-screen py-10 box-border font-mono">
+      <div className=" text-white m-8 text-xl">TodoList</div>
+      <div className="text-gray-700 sm:flex gap-8 items-center">
+        <TodoList />
+        <CreateTodo />
+      </div>
     </div>
   );
 }
