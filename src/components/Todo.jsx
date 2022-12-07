7f02b59860dd16e91bde69e5ef111da5276c8324
Dam <shit2wice@gmail.com>
2022-12-07 17:47:05 +0100

    style: Display list of all todos

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Todo = () => {
-  return <div>Todo</div>;
+  return (
+    <div className="flex justify-around p-4 rounded-xl bg-white text-gray-700 gap-4 my-4">
+      <div className="">This is the first todo</div>
+      <div className="text-blue-500">07/12/2022</div>
+      <div className="">x</div>
+    </div>
+  );
 };
 
 export default Todo;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Todo from "./Todo";
 
 const TodoList = () => {
-  return <div>TodoList</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <div>
+          total: <span className="text-white">2</span>
+        </div>
+        <select className="rounded-md p-2 outline-none">
+          <option>All</option>
+          <option className="">Sort by date</option>
+          <option>Sort by Title</option>
+        </select>
+      </div>
+      <div className="main">
+        <Todo />
+      </div>
+    </div>
+  );
 };
 
 export default TodoList;
